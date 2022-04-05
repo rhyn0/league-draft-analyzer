827933e830650272692706889365fed18eb52b9f
Ryan Ozawa <ryan.ozawa21@gmail.com>
2022-04-04 20:16:24 -0700

    add starting code

--- src/champions.py
+++ src/champions.py
@@ -9,4 +9,15 @@ class ChampionData:
     This is where a database of those traits will live and be transformed into machine data.
     """
 
+    """
+    Example:
+        Given Blue: Tryndamere, Viego, Zoe, Jhin, Leona
+        Given Red: Jax, Xin Zhao, Viktor, Jinx, Thresh
+
+        Blue Vector: <......., Tryndamere :yup:, .....> + (<Attack, Split, Siege, Protect, Catch> / 5)
+
+        Inputs to Machine:
+            Blue Vector: length 160
+            Red Vector: length 160
+    """
     pass
